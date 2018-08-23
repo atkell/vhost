a1f19a479d41b01956267d364532b380eb4681d4
A.T. Kell <alex@alexkell.com>
2018-08-23 13:16:17 -0400

    Creating the basic python file to get started

---
+++ vhost.py
@@ -0,0 +1,7 @@
+# we can do much when we put our mind to it, but for now let's practice KIS and start small
+# Objective #1 – To use python to create a new Apache2 virtualhost configuration file that accomplishes the following...
+# 	* Accepts agrument variables for domain, platform and web root
+# 	* Creates a new file, writes some content to this file, saves this file to the appropriate place (/etc/apache2/sites-available) and then closes it
+#	* Instructs Apache2 via a command to "enable" the new site with 'sudo a2ensite'
+
+
